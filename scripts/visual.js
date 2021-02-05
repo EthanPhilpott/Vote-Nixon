c17d5be438fa715b76a14d73c2df65754d288333
Ethan Philpott <ephilpott2004@gmail.com>
2021-02-05 05:39:39 -0700

    Worked more on the braching paths in js

--- scripts/visual.js
+++ scripts/visual.js
@@ -1,120 +1,60 @@
 "use strict"
 
 const READING_SPEED = 250 / 10
-let inProcess = false;
-let autoPlay = false;
 
-window.addEventListener('click', () => {
-    if (!inProcess) NextLine()
-})
-
-function WriteLetter (letter) {
-    textBody.innerHTML += letter;
+const script = {
+    START: `Today is my first day at West-Mec Highschool!
+    I am so exited to meet new poeple and make new freinds`,
+    TEST1: `This is a test branch, 
+    test branch one to be exact`,
+    TEST2: `This is a test branch,
+    test branch two to be exact`,
+    TEST3: `Try me
+    Nerd`
 }
 
-function NextLine () {
-    let si = script[index];
-    inProcess = true;
-    switch (si.type) {
-        case 'text':
-            textBody.innerHTML = '';
-            let dn = Date.now();
-            let i = 0;
-            let id = setInterval(() => {
-                if (i >= si.value.length) {
-                    if (autoPlay) {
-                        setTimeout(() => {
-                            index++;
-                            inProcess = false
-                        }, si.autoPlayPause);
-                    } else {
-                        index++;
-                        inProcess = false
-                    }
-                    clearInterval(id);
-                } else {
-                    WriteLetter(si.value[i])
-                    i++
-                }
-            }, READING_SPEED);
-            break;
-        case 'pause':
-            setTimeout(() => {
-                index++; 
-                inProcess = false;
-            }, si.value)
-            break;
-        case 'fadeText':
-            fadeBox.style.opacity = 0;
-            setTimeout(() => {
-                index++;
-                inProcess = false;
-            }, 500 + si.autoPlayPause);
-            break;
-        case 'revealText':
-            speaker.innerHTML = si.value;
-            fadeBox.style.opacity = 1;
-            setTimeout(() => {
-                index++;
-                inProcess = false
-            }, 500 + si.autoPlayPause);
-            break;
-        case 'choice':
-            inProcess = false;
-            index++;
-            break;
-        default:
-            inProcess = false;
-            index++;
-            break;
-    }
-}
-
-let line = 0;
-let index = 0;
-
-class Line {
-    constructor (value, autoPlayPause, target) {
-        this.value = value;
-        this.target = target;
-        this.autoPlayPause = autoPlayPause;
-    }
-    Execute () {
-        if (typeof this.value === "string") {
-            let id = setInterval(() => {
-                if (i >= si.value.length) {
-                    if (autoPlay) {
-                        setTimeout(() => {
-                            index++;
-                            inProcess = false
-                        }, si.autoPlayPause);
-                    } else {
-                        index++;
-                        inProcess = false
-                    }
-                    clearInterval(id);
-                } else {
-                    WriteLetter(this.value[i])
-                    i++
-                }
-            }, READING_SPEED);
-        } else if () {
-
-        }
-        this.Kill();
-    }
-    Kill () {
-        this.target.Execute()
-    }
-}
-
-
-
-const SCRIPT = [
-    new Line ("Hello World", 1000, 
-        new Line ("Nerd", null)
-    )   
+const REFERENCES = [
+    new Reference ('_root'        ,  0, null, START),
+    new Reference ("Go down Test1",  1,    0, TEST1),
+    new Reference ("Go down Test2",  2,    0, TEST2),
+    new Reference ("Go down Test3",  3,    1, SCRIPT.TEST3)
 ]
+
+// Major props to this article for the tree structure
+// https://typeofnan.dev/an-easy-way-to-build-a-tree-with-object-references/
+
+class Reference {
+    constructor (choice, id, parentId, script) {
+        this.choice = choice;
+        this.script = script;
+        this.children = [];
+        this.parentId = parentId;
+        this.id = id;
+    }
+}
+
+function Search (searchId) {
+    for (let obj of REFERENCES) {
+        if (obj.id === searchId)
+            return obj;
+    }
+}
+
+function FormTreeStruct () {
+    for (let ref of REFERENCES) {
+        if (ref.parentId !== null) {
+            Search(ref.parentId).children.push(ref);
+        } else {
+            treeRoot = ref;
+        }
+    }
+}
+
+let treeRoot;
+FormTreeStruct()
+console.log(treeRoot)
+
+
 
 
 
